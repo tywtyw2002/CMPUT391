5e1ac74c968b7168ccc576d849402ade44997445
UliceNix <mingxun@ualberta.ca>
2014-02-05 15:53:58 -0700

    Create data_populate.sql
    Some sample data to populate into database

---
+++ data_populate.sql
@@ -0,0 +1,66 @@
+insert into persons 
+values (1, 'Amy', 'Adams', '10006 112st, Edmonton, AB', 
+'a.adams@gmail.com', '7809991234');
+
+insert into persons 
+values (2, 'Mike', 'Evans', '10206 82ave, Edmonton, AB', 
+'mike.evans@gmail.com', '7801291234');
+insert into persons 
+values (3, 'Greg', 'Blake', '78 - 5th avenue, New York city,
+NY', 'gameboy123@hotmail.com', '5102445234');
+insert into persons 
+values (4, 'Cecily', 'Strong', '8H Rockefella center,
+NYC, NY', 'c.strong@nbc.com', '4319291252');
+
+insert into persons 
+values (5, 'Julie', 'Scully', '1203 11235 62ave, Edmonton,
+AB', 'JScully@live.com', '7804532212');
+insert into persons 
+values (6, 'Ariana', 'Huffington', '9999 100st, edmonton, ab'
+, 'ariana@huffingtonpost.com', '5792345678');
+insert into persons 
+values (7, 'Duke', 'Griffin', '31 spooner street, Quahog,
+RI', 'DukeGriffin@fox.com', '1238993412');
+insert into persons 
+values (8, 'Melisa', 'Hartman', '45 spooner street, Quahog,
+RI', 'MHartman@gmail.com', '6613425467');
+
+insert into persons 
+values (9, 'Jennifer', 'Tilly', '1003 11235 45ave, Edmonton,
+AB', 'JTilly@live.com', '7803512212');
+insert into persons 
+values (10, 'Merlin', 'Author', '9124 120st, Edmonton, AB'
+, 'Merlin@Camelot.com', '5793545678');
+insert into persons 
+values (11, 'Jennifer', 'Beal', '10089 7th avenue, Pittsburg,
+PA', 'JBeal@fourrooms.com', '9208993412');
+insert into persons 
+values (12, 'Connie', 'De''mico', '109 spooner street, Quahog,
+RI', 'Cdemico@gmail.com', '5713425467');
+
+insert into RADIOLOGY_RECORD
+values (1, 5, 4, 10, 'Atest', '01-DEC-2013', '05-DEC-2013', 'Looks fine!', 
+'Drink more!');
+insert into RADIOLOGY_RECORD 
+values (2, 5, 4, 10, 'Btest', '12-DEC-2013', '20-DEC-2013', 'Hmm...Okay?', 
+'Come back and see me in a week.');
+insert into RADIOLOGY_RECORD 
+values (3, 5, 4, 11, 'Ctest', '17-DEC-2013', '19-DEC-2013', 'This is bad!', 
+'Need another test!');
+insert into RADIOLOGY_RECORD 
+values (4, 5, 4, 9, 'Atest', '29-DEC-2013', '30-DEC-2013', 'Great!', 
+'MyMistake!');
+
+insert into RADIOLOGY_RECORD 
+values (5, 6, 2, 12, 'Atest', '18-JAN-2013', '21-FEB-2013', 'Fine by me', 
+'Good to go!');
+insert into RADIOLOGY_RECORD 
+values (6, 6, 2, 9, 'Btest', '12-MAY-2013', '30-JUN-2013', 'Great! Cured!', 
+'Nothing');
+insert into RADIOLOGY_RECORD 
+values (7, 7, 3, 11, 'Ctest', '22-APR-2013', '30-APR-2013', 
+'Can''t be optimistic', 'Faith');
+insert into RADIOLOGY_RECORD 
+values (8, 8, 2, 10, 'Atest', '02-FEB-2013', '01-MAR-2013', 'The cavity needs
+to be filled and may need other operations as well', 'Go back and see your 
+doctor please.');
